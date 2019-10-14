4eba0a3991a28ea2d389d0df3faccbed685a44eb
Crepp <tan.thai.2t@gmail.com>
2019-10-14 19:50:15 +0700

    complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function validateForm(){
-    var x = document.forms["myForm"]["number1"].value;
-    var y = document.forms["myForm"]["number2"].value;
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
     var checkPlus = document.getElementById("plus").checked;
     var checkMinus = document.getElementById("minus").checked;
     var checkMulti = document.getElementById("multi").checked;
@@ -25,12 +25,31 @@ function Caculate(){
     var checkMinus = document.getElementById("minus").checked;
     var checkMulti = document.getElementById("multi").checked;
     var checkDivide = document.getElementById("divide").checked;
-    var x = document.forms["myForm"]["number1"].value;
-    var y = document.forms["myForm"]["number2"].value;
-    var res;
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
+    x = parseFloat(x);
+    y = parseFloat(y);
     if(validateForm()){
         if(checkPlus){
             document.getElementById("result").innerHTML = x + y;
         }
+        if(checkMinus){
+            document.getElementById("result").innerHTML = x - y;
+        }
+        if(checkMulti){
+            document.getElementById("result").innerHTML = x * y;
+        }
+        if(checkDivide){
+            document.getElementById("result").innerHTML = x / y;
+        }
+    }
+}
+
+function focusOut(){
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
+    if(isNaN(x) || isNaN(y)){
+        alert("DỮ LIỆU NHẬP VÀO KHÔNG PHẢI SỐ THỰC!")
+        return false;
     }
 }
